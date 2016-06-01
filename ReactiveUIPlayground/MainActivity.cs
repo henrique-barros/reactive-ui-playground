91fa401e27acf263ed93066c4a5888f9c9ca0de2
henriquesb6 <henriquesb6@hotmail.com>
2016-06-01 01:00:20 -0300

    first commit

---
+++ ReactiveUIPlayground/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using ReactiveUIPlaygroundBusiness;
+using ReactiveUI;
+using System;
+
+namespace ReactiveUIPlayground
+{
+	[Activity(Label = "ReactiveUIPlayground", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity,IViewFor<MainViewModel>
+	{
+		int count = 1;
+
+		public MainViewModel viewModel;
+
+		public EditText editTextInput;
+		public TextView textViewOutput;
+		public Button buttonStartStop;
+		public Button buttonReset;
+		public TextView textViewCounter;
+
+		public MainViewModel ViewModel
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		object IViewFor.ViewModel
+		{
+			get
+			{
+				return viewModel;
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+
+			viewModel = new MainViewModel();
+
+			editTextInput = ((EditText)FindViewById(Resource.Id.editTextInput));
+			textViewOutput = (TextView)FindViewById(Resource.Id.textViewOutput);
+			this.Bind(viewModel, a => a.TextInput, asd => asd.editTextInput.Text);
+			this.Bind(viewModel, gsg => gsg.TextOutput, dsde => dsde.textViewOutput.Text);
+			this.Bi
+		}
+	}
+}
+
+
---
+++ ReactiveUIPlaygroundBusiness/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using ReactiveUI;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ReactiveUIPlaygroundBusiness
+{
+	public class MainViewModel: ReactiveObject
+	{
+		string _TextInput;
+		public string TextInput
+		{
+			get { return _TextInput; }
+			set {this.RaiseAndSetIfChanged(ref _TextInput, value); }
+		}
+
+		public ReactiveCommand StartStopCounting { get; private set;}
+
+		ObservableAsPropertyHelper<string> _TextOutput;
+		public string TextOutput => _TextOutput.Value;
+
+		bool _countDirection;
+		public bool CountDirection
+		{
+			get { return _countDirection; }
+			set { this._countDirection = value; }
+		}
+
+		ObservableAsPropertyHelper<string> _CounterText;
+		public string CounterText => _CounterText.Value;
+
+		/*
+		string _TextOutput;
+		public string TextOutput
+		{
+			get { return _TextOutput;}
+			set { this._TextOutput = value;}
+		}
+		*/
+
+		public MainViewModel()
+		{
+			_TextOutput = this.WhenAnyValue(x =>  x.TextInput).Select(y => y.ToUpper()).ToProperty(this, x=> x.TextOutput, "");
+
+			_CounterText = this.WhenAnyValue(x => x.CounterText).Select(text => (Convert.ToInt32(text) + 1).ToString()).ToProperty(
+				this, x => x.CounterText, "0");
+			                   //var startStopCountingObservable = this.WhenAnyValue(x => x.CountDirection, () ;
+		}
+
+
+	}
+}
+
